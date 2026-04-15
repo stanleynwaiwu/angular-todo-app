522c458fde8854b772c6b2cbc7a4f1cd0de5a4a4
Stanley Nwaiwu <nwaistanley@gmail.com>
2026-04-15 11:35:52 +0100

    feat: create people list component

---
+++ src/app/features/people/components/people-list/people-list.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-people-list',
+  imports: [],
+  templateUrl: './people-list.html',
+  styleUrl: './people-list.css',
+})
+export class PeopleList {}
