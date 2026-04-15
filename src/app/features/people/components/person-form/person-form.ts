c8a68e2272d03cde35980d65410e272b1496783b
Stanley Nwaiwu <nwaistanley@gmail.com>
2026-04-15 21:37:42 +0100

    feat: create person form component for modal

---
+++ src/app/features/people/components/person-form/person-form.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-person-form',
+  imports: [],
+  templateUrl: './person-form.html',
+  styleUrl: './person-form.css',
+})
+export class PersonForm {}
